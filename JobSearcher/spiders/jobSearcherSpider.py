80c852c1adffd32186f74035ee58af7b3313db25
Aniruddhsinh Chavda <aniruddhsinh.chavda03@gmail.com>
2020-06-08 10:58:17 +0530

    add comments

--- JobSearcher/spiders/jobSearcherSpider.py
+++ JobSearcher/spiders/jobSearcherSpider.py
@@ -2,39 +2,39 @@
 import scrapy
 
 
-class JonsearcherspiderSpider(scrapy.Spider):
+class JobsearcherspiderSpider(scrapy.Spider):
     name = 'jonSearcherSpider'
     allowed_domains = ['newyork.craigslist.org']
-    start_urls = ['https://newyork.craigslist.org/search/egr']
+    start_urls = ['https://newyork.craigslist.org/d/automotive-services/search/aos']
 
     def parse(self, response):
-        listings = response.xpath('//li[@class="result-row"]')
-        for listing in listings:
-            date = listing.xpath('.//*[@class="result-date"]/@datetime').extract_first()
-            link = listing.xpath('.//a[@class="result-title hdrlnk"]/@href').extract_first()
-            text = listing.xpath('.//a[@class="result-title hdrlnk"]/text()').extract_first()
+
+        # extract job urls from webpage
+        listOfJobLinks = response.xpath('//li[@class="result-row"]')
+        # extract all jobs details list one by one
+        for jobLink in listOfJobLinks:
+            date = jobLink.xpath('.//*[@class="result-date"]/@datetime').extract_first()
+            link = jobLink.xpath('.//a[@class="result-title hdrlnk"]/@href').extract_first()
+            text = jobLink.xpath('.//a[@class="result-title hdrlnk"]/text()').extract_first()
 
             yield scrapy.Request(link,
-                                 callback=self.parse_listing,
+                                 callback=self.parse_job_page,
                                  meta={'date': date,
                                        'link': link,
                                        'text': text})
-
+        # extract next page url and get webpage
         next_page_url = response.xpath('//a[text()="next > "]/@href').extract_first()
         if next_page_url:
             yield scrapy.Request(response.urljoin(next_page_url), callback=self.parse)
 
-    def parse_listing(self, response):
+    def parse_job_page(self, response):
         date = response.meta['date']
         link = response.meta['link']
         text = response.meta['text']
-
+        # extract deails of specific search
         compensation = response.xpath('//*[@class="attrgroup"]/span[1]/b/text()').extract_first()
         type = response.xpath('//*[@class="attrgroup"]/span[2]/b/text()').extract_first()
-
-        images = response.xpath('//*[@id="thumbs"]//@src').extract()
-        images = [image.replace('50x50c', '600x450') for image in images]
-
+        images = response.xpath('//*[@id="thumbs"]//@href').extract()
         address = response.xpath('//*[@id="postingbody"]/text()').extract()
 
         yield {'date': date,
